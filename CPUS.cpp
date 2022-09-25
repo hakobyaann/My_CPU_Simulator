5df4c3837f3bb346d2db6957d953f1db51ae4d75
hakobyaann <109814279+hakobyaann@users.noreply.github.com>
2022-09-26 02:13:49 +0400

    CPUS.cpp

--- CPUS.cpp
+++ CPUS.cpp
@@ -4,7 +4,8 @@
 #include <map>
 
 // Function Prototypes
-void read(std::ifstream);
+void read();
+void direction(std::string, std::string&, std::string&);
 void mov(std::string&, std::string&);
 void add(std::string&, std::string&);
 void sub(std::string&, std::string&);
@@ -16,95 +17,65 @@ void cmp(std::string&, std::string&);
 // Registers
 struct Register
 {
-  int a1;
-  int a2;
-  int a3;
-  int a4;
-  int a5;
-  int a6;
-  int a7;
-  int a8;
-  int a9;
+  int a1 = 0;
+  int a2 = 0;
+  int a3 = 0;
+  int a4 = 0;
+  int a5 = 0;
+  int a6 = 0;
+  int a7 = 0;
+  int a8 = 0;
+  int a9 = 0;
   int a10 = 0;
-  std::map<std::string, int>R;
 } Reggy;
 
+//The entry point
 int main()
 {
-    R["a1"] = Register::a1;
-    R["a2"] = Register::a2;
-    R["a3"] = Register::a3;
-    R["a4"] = Register::a4;
-    R["a5"] = Register::a5;
-    R["a6"] = Register::a6;
-    R["a7"] = Register::a7;
-    R["a8"] = Register::a8;
-    R["a9"] = Register::a9;
-    R["a10"] = Register::a10;
-    std::ifstream inFile("file.txt");
+  read();
+  std::cout << Reggy.a1 << std::endl;;
+  std::cout << Reggy.a2 << std::endl;
+  std::cout << Reggy.a3 << std::endl;
+  return 0;
+}
+
+void read()
+{
+  std::string token;
+  std::string argument1;
+  std::string argument2;
+  std::ifstream inFile("file.txt");
   if (inFile.fail())
   {
     std::cout << "Unable to open file for reading." << std::endl;
     exit(1);
   }
-  read(inFile);
-  inFile.close();
-  return 0;
-}
-
-void read(std::ifstream inFile)
-{
-  std::string token;
-  std::string argument1;
-  std::string argument2;
-  std::map<std::string, std::string (*)(std::sring)> Box;
-  Box["mov"] = &mov;
-  Box["add"] = &add;
-  Box["sub"] = &sub;
-  Box["mul"] = &mul;
-  Box["inc"] = &inc;
-  Box["dec"] = &dec;
-  Box["cmp"] = &cmp;
-
-  while (!inFile.EOF())
+    while (!inFile.eof())
   {
     inFile >> token;
     if (token == "mov" || token == "add" || token == "sub" || token == "mul" || token == "inc" || token == "dec" || token == "cmp")
     {
-      inFile >> argument1;
-      inFile >> argument2;
-      Box[token](argument1, argument2);
-    }
-    else
-    {
-      std::cout << "There is no such opcode!" << std::endl;
-      return;
+        inFile >> argument1;
+        inFile >> argument2;
+        direction(token, argument1, argument2);
     }
   }
-  /* std::string nextToken;
-   while (inFile >> nextToken) {
-     std::cout << "Token: " << nextToken << std::endl;
-  }*/
+        inFile.close();
 }
+
+void direction(std::string token, std::string& argument1, std::string& argument2)
+{
+        if (token == "mov")
+        {
+          mov(argument1, argument2);
+        }
+
+}
+
 
 void mov(std::string &argument1, std::string &argument2)
 {
-   if (R.find(argument1) == R.end())
-   {
-      std::cout << "There is no such "
-   }
-   if (R.find(argument2) == R.end())
-   {
-      *R[argument1] = stoi(argument2);
-   }
-   else
-   {
-      *R[argument1] = *R[argument2];
-   }
-
-
-
-   /* if (argument1 == "a1,")
+    if (argument1 == "a1,")
     {
       Reggy.a1 = stoi(argument2);
     }
@@ -144,6 +115,10 @@ void mov(std::string &argument1, std::string &argument2)
     {
       Reggy.a10 = stoi(argument2);
     }
-    
-  }*/
 }
+                                                                                                                              115,1         Bot
+
+
+                                                                                                                              90,1          75%
+
+
